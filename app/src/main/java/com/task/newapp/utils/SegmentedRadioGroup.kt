0263c41d914f3e90f563c788e33388ca4890bc72
hardikadbiz <hardikadbiz@gmail.com>
2021-06-23 10:05:35 +0530

    Registration step 2 - hardik

---
+++ app/src/main/java/com/task/newapp/utils/SegmentedRadioGroup.kt
@@ -0,0 +1,26 @@
+package com.task.newapp.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RadioGroup
+import com.task.newapp.R
+
+class SegmentedRadioGroup : RadioGroup {
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        changeButtonsImages()
+    }
+
+    private fun changeButtonsImages() {
+        val count = super.getChildCount()
+        if (count > 1) {
+            super.getChildAt(0)
+                .setBackgroundResource(R.drawable.segment_radio_left)
+            super.getChildAt(1)
+                .setBackgroundResource(R.drawable.segment_radio_right)
+        }
+    }
+}
