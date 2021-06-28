f71e6802213bbc6818414c9859e322b13e4ee375
hardikadbiz <hardikadbiz@gmail.com>
2021-06-28 11:01:02 +0530

    Main Screen done

--- app/src/main/java/com/task/newapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/task/newapp/ui/activities/MainActivity.kt
@@ -7,9 +7,13 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.task.newapp.R
 import com.task.newapp.databinding.ActivityMainBinding
+import com.task.newapp.ui.fragments.registration.ChatsFragment
+import com.task.newapp.ui.fragments.registration.RegistrationStep1Fragment
 import com.task.newapp.utils.setBlurLayout
 import com.task.newapp.utils.showToast
 import com.task.newapp.utils.setBlurLayout
@@ -17,8 +21,14 @@ import com.task.newapp.utils.showToast
 import eightbitlab.com.blurview.RenderScriptBlur
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
     lateinit var binding: ActivityMainBinding
+
+    val chatsFragment = ChatsFragment()
+    val secondFragment = ChatsFragment()
+    val thirdFragment = ChatsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -55,29 +65,81 @@ class MainActivity : AppCompatActivity() {
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
-        //        binding.bottomBar.setActiveItem(1)
+//        binding.bottomBar.setActiveItem(1)
 //        binding.bottomBar.setBadge(2)
 //        binding.bottomBar.removeBadge(2)
 
-//        binding.bottomBar.onItemSelected = {
-//            showToast("Item $it selected")
-//        }
-//
-//        binding.bottomBar.onItemReselected = {
-//            showToast("Item $it re-selected")
-//        }
+        binding.bottomBar.onItemSelected = { position ->
+            showToast("Item $position selected")
+            setCurrentFragment(chatsFragment)
+
+            when (position) {
+                0 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                1 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                2 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                3 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                4 -> {
+                    toggleRightDrawer()
+                }
+            }
+        }
+
+        binding.bottomBar.onItemReselected = { position ->
+            showToast("Item $position re-selected")
+
+            when (position) {
+                0 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                1 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                2 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                3 -> {
+                    setCurrentFragment(chatsFragment)
+                }
+                4 -> {
+                    toggleRightDrawer()
+                }
+            }
+        }
 //
 //        binding.bottomBar.onItemLongClick = {
 //            showToast("Item $it long click")
 //        }
 
+        setCurrentFragment(chatsFragment)
     }
 
     private fun toggleRightDrawer() {
-        if (binding.drawerLayout.isDrawerOpen(binding.drawerLayout)) {
-            binding.drawerLayout.closeDrawer(binding.drawerLayout)
+        if (binding.drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            binding.drawerLayout.closeDrawer(GravityCompat.END)
         } else {
-            binding.drawerLayout.openDrawer(binding.drawerLayout)
+            binding.drawerLayout.openDrawer(GravityCompat.END)
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.activity_main_content, fragment)
+            commit()
+        }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.img_center -> {
+
+            }
         }
     }
 }
